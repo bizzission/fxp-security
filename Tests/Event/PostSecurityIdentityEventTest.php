01d351505381d69a75f878a2f00b6cbbfe1084a5
François Pluchino <francois.pluchino@sonatra.com>
2016-11-11 17:36:51 +0100

    Add post security identity event tests

---
+++ Tests/Event/PostSecurityIdentityEventTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Component\Security\Tests\Event;
+
+use Sonatra\Component\Security\Event\PostSecurityIdentityEvent;
+use Symfony\Component\Security\Acl\Model\SecurityIdentityInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class PostSecurityIdentityEventTest extends \PHPUnit_Framework_TestCase
+{
+    public function testEvent()
+    {
+        /* @var TokenInterface $token */
+        $token = $this->getMockBuilder(TokenInterface::class)->getMock();
+        $sids = array(
+            $this->getMockBuilder(SecurityIdentityInterface::class)->getMock(),
+        );
+
+        $event = new PostSecurityIdentityEvent($token, $sids);
+
+        $this->assertSame($token, $event->getToken());
+        $this->assertSame($sids, $event->getSecurityIdentities());
+        $this->assertTrue($event->isAclEnabled());
+    }
+}
