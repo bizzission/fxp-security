c1118722825b4564b972aff9dc0b62a0efc409f8
François Pluchino <francois.pluchino@sonatra.com>
2016-11-11 14:37:18 +0100

    Add organization model tests

---
+++ Tests/Model/OrganizationTest.php
@@ -0,0 +1,152 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Component\Security\Tests\Model;
+
+use Sonatra\Component\Security\Model\GroupInterface;
+use Sonatra\Component\Security\Model\OrganizationUserInterface;
+use Sonatra\Component\Security\Model\UserInterface;
+use Sonatra\Component\Security\Tests\Fixtures\Model\MockOrganization;
+use Sonatra\Component\Security\Tests\Fixtures\Model\MockRole;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class OrganizationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testModel()
+    {
+        $org = new MockOrganization('FOO');
+
+        $this->assertNull($org->getId());
+        $this->assertSame('FOO', $org->getName());
+        $this->assertNull($org->getUser());
+        $this->assertFalse($org->isUserOrganization());
+        $this->assertCount(0, $org->getOrganizationRoles());
+        $this->assertCount(0, $org->getOrganizationRoleNames());
+        $this->assertFalse($org->hasOrganizationRole('ROLE_ADMIN'));
+        $this->assertCount(0, $org->getOrganizationGroups());
+        $this->assertCount(0, $org->getOrganizationGroupNames());
+        $this->assertFalse($org->hasOrganizationGroup('GROUP_DEFAULT'));
+        $this->assertCount(0, $org->getOrganizationUsers());
+        $this->assertCount(0, $org->getOrganizationUserNames());
+        $this->assertFalse($org->hasOrganizationUser('user.test'));
+        $this->assertSame('FOO', (string) $org);
+    }
+
+    public function testModelName()
+    {
+        $org = new MockOrganization('FOO');
+
+        $this->assertSame('FOO', $org->getName());
+        $org->setName('BAR');
+        $this->assertSame('BAR', $org->getName());
+    }
+
+    public function testModelUser()
+    {
+        /* @var UserInterface $user */
+        $user = $this->getMockBuilder(UserInterface::class)->getMock();
+        $org = new MockOrganization('FOO');
+
+        $this->assertNull($org->getUser());
+        $this->assertFalse($org->isUserOrganization());
+
+        $org->setUser($user);
+        $this->assertSame($user, $org->getUser());
+        $this->assertTrue($org->isUserOrganization());
+    }
+
+    public function testModelRoles()
+    {
+        $role = new MockRole('ROLE_ADMIN');
+        $org = new MockOrganization('FOO');
+
+        $this->assertCount(0, $org->getOrganizationRoles());
+        $this->assertCount(0, $org->getOrganizationRoleNames());
+        $this->assertFalse($org->hasOrganizationRole('ROLE_ADMIN'));
+
+        $org->addOrganizationRole($role);
+
+        $this->assertCount(1, $org->getOrganizationRoles());
+        $this->assertCount(1, $org->getOrganizationRoleNames());
+        $this->assertTrue($org->hasOrganizationRole('ROLE_ADMIN'));
+        $this->assertSame('ROLE_ADMIN', current($org->getOrganizationRoleNames()));
+
+        $org->removeOrganizationRole($role);
+
+        $this->assertCount(0, $org->getOrganizationRoles());
+        $this->assertCount(0, $org->getOrganizationRoleNames());
+        $this->assertFalse($org->hasOrganizationRole('ROLE_ADMIN'));
+    }
+
+    public function testModelGroups()
+    {
+        /* @var GroupInterface|\PHPUnit_Framework_MockObject_MockObject $group */
+        $group = $this->getMockBuilder(GroupInterface::class)->getMock();
+        $group->expects($this->any())
+            ->method('getName')
+            ->willReturn('GROUP_DEFAULT');
+
+        $org = new MockOrganization('FOO');
+
+        $this->assertCount(0, $org->getOrganizationGroups());
+        $this->assertCount(0, $org->getOrganizationGroupNames());
+        $this->assertFalse($org->hasOrganizationRole('GROUP_DEFAULT'));
+
+        $org->addOrganizationGroup($group);
+
+        $this->assertCount(1, $org->getOrganizationGroups());
+        $this->assertCount(1, $org->getOrganizationGroupNames());
+        $this->assertTrue($org->hasOrganizationGroup('GROUP_DEFAULT'));
+        $this->assertSame('GROUP_DEFAULT', current($org->getOrganizationGroupNames()));
+
+        $org->removeOrganizationGroup($group);
+
+        $this->assertCount(0, $org->getOrganizationGroups());
+        $this->assertCount(0, $org->getOrganizationGroupNames());
+        $this->assertFalse($org->hasOrganizationGroup('GROUP_DEFAULT'));
+    }
+
+    public function testModelUsers()
+    {
+        /* @var UserInterface|\PHPUnit_Framework_MockObject_MockObject $user */
+        $user = $this->getMockBuilder(UserInterface::class)->getMock();
+        $user->expects($this->any())
+            ->method('getUsername')
+            ->willReturn('user.test');
+
+        /* @var OrganizationUserInterface|\PHPUnit_Framework_MockObject_MockObject $orgUser */
+        $orgUser = $this->getMockBuilder(OrganizationUserInterface::class)->getMock();
+        $orgUser->expects($this->any())
+            ->method('getUser')
+            ->willReturn($user);
+
+        $org = new MockOrganization('FOO');
+
+        $this->assertCount(0, $org->getOrganizationUsers());
+        $this->assertCount(0, $org->getOrganizationUserNames());
+        $this->assertFalse($org->hasOrganizationUser($user->getUsername()));
+
+        $org->addOrganizationUser($orgUser);
+
+        $this->assertCount(1, $org->getOrganizationUsers());
+        $this->assertCount(1, $org->getOrganizationUserNames());
+        $this->assertTrue($org->hasOrganizationUser($user->getUsername()));
+        $this->assertSame($user->getUsername(), current($org->getOrganizationUserNames()));
+
+        $org->removeOrganizationUser($orgUser);
+
+        $this->assertCount(0, $org->getOrganizationUsers());
+        $this->assertCount(0, $org->getOrganizationUserNames());
+        $this->assertFalse($org->hasOrganizationUser($user->getUsername()));
+    }
+}
