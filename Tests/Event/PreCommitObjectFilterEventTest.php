a8bdf533c9a59658a59f6f88bdf8d81c5bbffa2d
François Pluchino <francois.pluchino@sonatra.com>
2016-11-11 15:54:10 +0100

    Add pre commit object filter event tests

---
+++ Tests/Event/PreCommitObjectFilterEventTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Component\Security\Tests\Event;
+
+use Sonatra\Component\Security\Event\PreCommitObjectFilterEvent;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class PreCommitObjectFilterEventTest extends \PHPUnit_Framework_TestCase
+{
+    public function testEvent()
+    {
+        $objects = array(
+            new \stdClass(),
+            new \stdClass(),
+            new \stdClass(),
+        );
+
+        $event = new PreCommitObjectFilterEvent($objects);
+        $this->assertSame($objects, $event->getObjects());
+    }
+}
