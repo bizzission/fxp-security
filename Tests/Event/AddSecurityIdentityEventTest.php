9bcfc00088a16fd4f5fb8b54ef37a9633b3d53b6
François Pluchino <francois.pluchino@sonatra.com>
2016-11-11 17:44:47 +0100

    Add add security identity event tests

---
+++ Tests/Event/AddSecurityIdentityEventTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Component\Security\Tests\Event;
+
+use Sonatra\Component\Security\Event\AddSecurityIdentityEvent;
+use Symfony\Component\Security\Acl\Model\SecurityIdentityInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class AddSecurityIdentityEventTest extends \PHPUnit_Framework_TestCase
+{
+    public function testEvent()
+    {
+        /* @var TokenInterface $token */
+        $token = $this->getMockBuilder(TokenInterface::class)->getMock();
+        $sids = array(
+            $this->getMockBuilder(SecurityIdentityInterface::class)->getMock(),
+        );
+
+        $event = new AddSecurityIdentityEvent($token, $sids);
+
+        $this->assertSame($token, $event->getToken());
+        $this->assertSame($sids, $event->getSecurityIdentities());
+
+        $sids[] = $this->getMockBuilder(SecurityIdentityInterface::class)->getMock();
+        $event->setSecurityIdentities($sids);
+
+        $this->assertSame($sids, $event->getSecurityIdentities());
+    }
+}
