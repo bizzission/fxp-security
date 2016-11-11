832d897a17cb2775743328197d44387f2dc69da8
François Pluchino <francois.pluchino@sonatra.com>
2016-11-11 17:45:12 +0100

    Add object view granted event tests

---
+++ Tests/Event/ObjectViewGrantedEventTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Component\Security\Tests\Event;
+
+use Sonatra\Component\Security\Event\ObjectViewGrantedEvent;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class ObjectViewGrantedEventTest extends \PHPUnit_Framework_TestCase
+{
+    public function testEvent()
+    {
+        $object = new \stdClass();
+        $event = new ObjectViewGrantedEvent($object);
+
+        $this->assertSame($object, $event->getObject());
+        $this->assertFalse($event->isSkipAuthorizationChecker());
+        $this->assertTrue($event->isGranted());
+
+        $event->setGranted(false);
+        $this->assertTrue($event->isSkipAuthorizationChecker());
+        $this->assertFalse($event->isGranted());
+    }
+}
