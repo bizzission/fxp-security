5d8a5c97dcbaa0b4b9e43e30be320271755b037b
François Pluchino <francois.pluchino@sonatra.com>
2016-11-16 18:40:18 +0100

    Add base of permission manager

---
+++ Permission/PermissionManager.php
@@ -0,0 +1,99 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Component\Security\Permission;
+
+use Sonatra\Component\Security\Identity\SecurityIdentityRetrievalStrategyInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+/**
+ * Permission manager.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class PermissionManager implements PermissionManagerInterface
+{
+    /**
+     * @var SecurityIdentityRetrievalStrategyInterface
+     */
+    protected $sidRetrievalStrategy;
+
+    /**
+     * @var bool
+     */
+    protected $enabled = true;
+
+    /**
+     * Constructor.
+     *
+     * @param SecurityIdentityRetrievalStrategyInterface $sidRetrievalStrategy The security identity retrieval strategy
+     */
+    public function __construct(SecurityIdentityRetrievalStrategyInterface $sidRetrievalStrategy)
+    {
+        $this->sidRetrievalStrategy = $sidRetrievalStrategy;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function enable()
+    {
+        $this->enabled = true;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function disable()
+    {
+        $this->enabled = false;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSecurityIdentities(TokenInterface $token = null)
+    {
+        if (null === $token) {
+            return array();
+        }
+
+        return $this->sidRetrievalStrategy->getSecurityIdentities($token);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function preloadPermissions(array $objects)
+    {
+        return new \SplObjectStorage();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resetPreloadPermissions(array $objects)
+    {
+        return $this;
+    }
+}
