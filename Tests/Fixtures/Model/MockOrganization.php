e837cbf5ee62d1313c6e7b6a2995e09055af1be7
François Pluchino <francois.pluchino@sonatra.com>
2016-11-19 13:03:27 +0100

    Update organization fixture

--- Tests/Fixtures/Model/MockOrganization.php
+++ Tests/Fixtures/Model/MockOrganization.php
@@ -12,13 +12,21 @@
 namespace Sonatra\Component\Security\Tests\Fixtures\Model;
 
 use Sonatra\Component\Security\Model\Organization;
+use Sonatra\Component\Security\Model\Traits\OrganizationGroupsInterface;
+use Sonatra\Component\Security\Model\Traits\OrganizationGroupsTrait;
+use Sonatra\Component\Security\Model\Traits\OrganizationRolesInterface;
+use Sonatra\Component\Security\Model\Traits\OrganizationRolesTrait;
 use Sonatra\Component\Security\Model\Traits\RoleableInterface;
 use Sonatra\Component\Security\Model\Traits\RoleableTrait;
 
 /**
  * @author François Pluchino <francois.pluchino@sonatra.com>
  */
-class MockOrganization extends Organization implements RoleableInterface
+class MockOrganization extends Organization implements RoleableInterface,
+    OrganizationRolesInterface,
+    OrganizationGroupsInterface
 {
     use RoleableTrait;
+    use OrganizationRolesTrait;
+    use OrganizationGroupsTrait;
 }
